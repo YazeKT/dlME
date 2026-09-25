eae966de491c36d0590b092307c10105c09a77cf
Kirsten Trimaley <62893829+YazeKT@users.noreply.github.com>
2026-09-25 16:34:38 +0200

    Publish dlME 1.1.0 release metadata

--- website/site-data.js
+++ website/site-data.js
@@ -1,13 +1,13 @@
 window.DLME_SITE_DATA = {
   "version": "1.1.0",
-  "publishedVersion": "1.0.0",
-  "releaseReady": false,
+  "publishedVersion": "1.1.0",
+  "releaseReady": true,
   "sourceVersion": "1.1.0",
   "engineVersion": "2026.08.19",
   "extractorEntries": 1752,
-  "testCount": 50,
+  "testCount": 52,
   "auditFindings": 0,
   "runtimeCount": 5,
-  "releaseAssetCount": 12,
-  "releaseUrl": "https://github.com/YazeKT/dlME/releases/tag/v1.0.0"
+  "releaseAssetCount": 8,
+  "releaseUrl": "https://github.com/YazeKT/dlME/releases/tag/v1.1.0"
 };
